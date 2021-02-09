eea23c0e1512a656943eec8168cd1b7b037aa20c
Christopher Choi <christopherchoi@christophersmbp.lan>
2021-02-08 22:45:20 -0600

    adding state to sign in component

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Button from './basic-elements/Button';
 import '../styles/sign-in.css';
 import '../styles/text.css';
 
+function onSignInClick() {
+  console.log('clicked!');
+}
+
 function SignIn() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
   return (
     <div className="sign-in-container text-subheader">
       <div className="sign-in-card">
@@ -17,7 +23,9 @@ function SignIn() {
           <label htmlFor="password">Password</label>
           <input type="text" name="password" id="password" />
         </div>
-        <Button text="Sign in" />
+        <div className="sign-in-button">
+          <Button text="Sign in" onClick={onSignInClick} />
+        </div>
         <div className="sign-in-create-account text-paragraph">
           <p>Don&#39;t have an account?</p>
           <Link to="/sign-in">Create account</Link>
--- src/components/basic-elements/Button.tsx
+++ src/components/basic-elements/Button.tsx
@@ -2,24 +2,17 @@ import React from 'react';
 import '../../styles/button.css';
 import '../../styles/text.css';
 
-enum ButtonWidthOptions {
-  SMALL = 'small',
-  MEDIUM = 'medium',
-  LARGE = 'large',
-}
-
 type ButtonProps = {
   text: string,
-  width: ButtonWidthOptions,
   onClick?: () => void,
 }
 
-function Button({ text, width, onClick }: ButtonProps) {
+function Button({ text, onClick }: ButtonProps) {
   return (
     <button
       type="button"
       className="button text-nav"
-      onClick={onClick} 
+      onClick={onClick}
     >
       {text}
     </button>
